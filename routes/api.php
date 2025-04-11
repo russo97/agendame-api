cbc5068fc514c919960c1dd7149e4f1def8f2dda
Wanderson Silva <wandersonsilva768@gmail.com>
2025-04-10 21:58:46 -0300

    feat: inclui endpoint responsável por verificar e retornar os dados do usuário autenticado

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\User\MeController;
 use App\Http\Controllers\Auth\LoginController;
 
 Route::post('login', LoginController::class);
+
+Route::middleware(['auth:sanctum']) -> group(function () {
+  Route::get('me', [MeController::class, 'show']);
+});
